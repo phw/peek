3692865419b0c94e292f714438e81ad603f13e82
Philipp Wolfer <phil@parolu.io>
2017-03-14 09:06:33 +0100

    Flatpak: Build architectures in separate build directories
    Otherwise caching does not work (cache gets overwritten by

--- data/flatpak/build.sh
+++ data/flatpak/build.sh
@@ -7,12 +7,20 @@ else
     BRANCH=$1
 fi
 
-cd `dirname $0`
+WORK_DIR=$(dirname $0)
+cd $WORK_DIR
 
-flatpak-builder --repo="${LOCAL_REPO}" com.uploadedlobster.peek \
-  --gpg-sign=B539AD7A5763EE9C1C2E4DE24C14923F47BF1A02 \
-  flatpak-${BRANCH}.json --force-clean --arch=x86_64
+build_for_arch () {
+  ARCH=$1
+  mkdir -p .${ARCH}
+  cd .${ARCH}
+  cp ../flatpak-${BRANCH}.json .
 
-flatpak-builder --repo="${LOCAL_REPO}" com.uploadedlobster.peek \
-  --gpg-sign=B539AD7A5763EE9C1C2E4DE24C14923F47BF1A02 \
-  flatpak-${BRANCH}.json --force-clean --arch=i386
+  flatpak-builder --repo="${LOCAL_REPO}" com.uploadedlobster.peek \
+    --gpg-sign=B539AD7A5763EE9C1C2E4DE24C14923F47BF1A02 \
+    flatpak-${BRANCH}.json --force-clean --arch=${ARCH}
+  cd ..
+}
+
+build_for_arch x86_64
+build_for_arch i386
