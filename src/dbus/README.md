0b61760223817f57578ec1063b18925953e2945e
Philipp Wolfer <phil@parolu.io>
2018-03-18 15:20:36 +0100

    Added sources for DBus interfaces
    Fixes #296

---
+++ src/dbus/README.md
@@ -0,0 +1,10 @@
+# Generated DBus interfaces
+
+## org.Freedesktop.DBus
+Source: `dbus-send --print-reply=literal --dest=org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.Introspectable.Introspect`
+
+## org.freedesktop.FileManager1
+Source: https://www.freedesktop.org/wiki/Specifications/file-manager-interface/
+
+## org.gnome.Shell.Screencast.xml
+Source: https://gitlab.gnome.org/GNOME/gnome-shell/tree/master/data
---
+++ src/dbus/generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+WORKDIR=$(dirname $0)
+cd ${WORKDIR}
+
+vala-dbus-binding-tool --api-path=./org.gnome.Shell.Screencast.xml --directory=./ --strip-namespace=org --rename-namespace=gnome:Gnome --no-synced
+mv gnome-shell.vala gnome-shell-screencast.vala
+
+vala-dbus-binding-tool --api-path=./org.freedesktop.FileManager1.xml --directory=./ --strip-namespace=org --rename-namespace=freedesktop:Freedesktop --no-synced
+mv freedesktop.vala freedesktop-filemanager.vala
+
+vala-dbus-binding-tool --api-path=./org.freedesktop.DBus.xml --directory=./ --strip-namespace=org --rename-namespace=freedesktop:Freedesktop --no-synced
+mv freedesktop.vala freedesktop-dbus.vala
